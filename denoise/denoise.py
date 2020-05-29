fb86fb1439780c99b06da441a1480a192fce2663
Karel van Klink <karel@deob.us>
2020-05-29 16:44:48 +0200

    improved denoise performance on different pictures

--- denoise/denoise.py
+++ denoise/denoise.py
@@ -1,11 +1,13 @@
 import cv2 as cv
 
-img_color = cv.imread('/home/karel/Downloads/photo.jpg')
+img_color = cv.imread('/Users/kyle/Desktop/test.png')
 img_gray = cv.cvtColor(img_color, cv.COLOR_BGR2GRAY)
+cv.normalize(img_gray,  img_gray, 0, 165, cv.NORM_MINMAX)
 
 # is dit netjes?
 img_gray = cv.bitwise_not(img_gray)
 # nee
+_, img_gray = cv.threshold(img_gray, 255, 0, cv.THRESH_TRUNC)
 _, img = cv.threshold(img_gray, 127, 0, cv.THRESH_TOZERO)
 # werkt het?
 img = cv.bitwise_not(img)

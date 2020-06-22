09074c79641b0de0f30792c04e2cf0ed89784a18
Karel van Klink <karel@deob.us>
2020-06-22 12:43:21 +0200

    Rework template_matching.py
    add type definitions to method calls
    add method that takes an array of templates
    add arrays of templates per template kind
    remove call to .value in template_matching method since it only takes Template instances now

--- template_matching/template_matching.py
+++ template_matching/template_matching.py
@@ -5,13 +5,14 @@ import imutils
 import numpy as np
 
 from notes.note_objects import Template
+from staffs.staff_objects import Staff
 
 
-def template_matching(template, staff, threshold):
+def template_matching(template: Template, staff: Staff, threshold: float):
     img_gray = cv2.cvtColor(staff.image, cv2.COLOR_BGR2GRAY)
 
     # Resize template to match staff height
-    resized_template = imutils.resize(template.value.image, height=int(staff.dist * template.value.height_units))
+    resized_template = imutils.resize(template.image, height=int(staff.dist * template.height_units))
     results = cv2.matchTemplate(img_gray, resized_template, cv2.TM_CCOEFF_NORMED)
     locations = np.where(results >= threshold)
 
@@ -33,11 +34,20 @@ def template_matching(template, staff, threshold):
     return unique_matches
 
 
+def template_matching_array(templates: [Template], staff: Staff, threshold: float):
+    result = {}
+    for template in templates:
+        result[template.name] = template_matching(template, staff, threshold)
+    return result
+
+
 class AvailableTemplates(Enum):
     # Notes
     NoteheadClosed = Template('closed_notehead', 'images/templates/head-filled.png', 1)
     NoteheadOpen = Template('open_notehead', 'images/templates/head-open.jpg', 1)
     FlagUpsideDown = Template('flag_upside_down', 'images/templates/flag.png', 1.5)
+
+    AllNotes = [NoteheadClosed.value, NoteheadOpen.value, FlagUpsideDown.value]
 
     # Rests
     RestFull = Template('full_rest', 'images/templates/full-rest-on-line.jpg', 1)
@@ -47,10 +57,14 @@ class AvailableTemplates(Enum):
     RestFourth = Template('fourth_rest', 'images/templates/4th-rest-with-lines.jpg', 4)
     RestEighth = Template('eighth_rest', 'images/templates/8th-rest-with-line.jpg', 2)
 
+    AllRests = [RestFull.value, RestHalf.value, RestFourth.value, RestEighth.value]
+
     # Clefs
     ClefG = Template('g-clef', 'images/templates/g-clef-with-lines.jpg', 7.5)
     ClefF = Template('f-clef', 'images/templates/f-clef-with-lines.jpg', 4)
     ClefC = Template('c-clef', 'images/templates/c-clef-with-lines.jpg', 4)
+
+    AllClefs = [ClefG.value, ClefF.value, ClefC.value]
 
     # Keys
     Flat = Template('flat', 'images/templates/flat.jpg', 2.4)
@@ -59,8 +73,12 @@ class AvailableTemplates(Enum):
     SharpDouble = Template('double_sharp', 'images/templates/double-sharp.jpg', 1)
     Natural = Template('natural', 'images/templates/natural.jpg', 3)
 
+    AllKeys = [Flat.value, FlatDouble.value, Sharp.value, SharpDouble.value, Natural.value]
+
     # Symbols
     Fermate = Template('fermate', 'images/templates/fermate.jpg', 1.5)
+
+    AllSymbols = [Fermate.value]
 
     # Times
     Time3_4 = Template('3/4 time', 'images/templates/times/3_4.jpg', 4)
@@ -75,3 +93,6 @@ class AvailableTemplates(Enum):
     Time12_8 = Template('12/8 time', 'images/templates/times/12_8.jpg', 4)
     TimeAllaBreve = Template('alla breve', 'images/templates/times/alla_breve.jpg', 4)
     TimeC = Template('4/4 time C', 'images/templates/times/c.jpg', 4)
+
+    AllTimes = [Time3_4.value, Time3_8.value, Time4_4.value, Time5_4.value, Time5_8.value, Time6_4.value,
+                Time6_8.value, Time7_8.value, Time9_8.value, Time12_8.value, TimeAllaBreve.value, TimeC.value]

4ba202986f55b28dcf49eb9119f3f71dc02dfd0a
Karel van Klink <karel@deob.us>
2020-07-10 15:10:37 +0200

    Use Otsu's method for denoise instead

--- denoise/denoise.py
+++ denoise/denoise.py
@@ -6,12 +6,11 @@ import cv2 as cv
 def denoise(img, is_rgb=False):
     if is_rgb:
         img = cv.cvtColor(img, cv.COLOR_RGB2GRAY)
+
     cv.normalize(img, img, 0, 165, cv.NORM_MINMAX)
-    img = cv.bitwise_not(img)
-    _, img = cv.threshold(img, 255, 0, cv.THRESH_TRUNC)
-    _, img = cv.threshold(img, 127, 0, cv.THRESH_TOZERO)
-    img = cv.bitwise_not(img)
+    _, img = cv.threshold(img, 0, 255, cv.THRESH_OTSU)
     img = cv.fastNlMeansDenoising(img, None, 20, 7, 21)
+
     if is_rgb:
         img = cv.cvtColor(img, cv.COLOR_GRAY2RGB)
     return img
@@ -22,7 +21,7 @@ def denoise_at(path, is_rgb=False):
 
 
 if __name__ == "__main__":
-    cv.imshow('output', denoise_at('/Users/kyle/Desktop/test.png'))
+    cv.imshow('output', denoise_at('../images/sheets/mscd-15/input.png'))
     cv.waitKey(0)
     cv.destroyAllWindows()
     cv.waitKey(1)

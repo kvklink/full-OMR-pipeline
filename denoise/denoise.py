6341002e14eaed65da9f1598c749c7ee9c46edd6
Karel van Klink <karel@deob.us>
2020-07-10 14:17:54 +0200

    reformat denoise and dewarp
    change isRgb flag to is_rgb in denoise.py
    change isRgb flag to is_rgb in dewarp.py
    add whitespace to denoise.py
    add whitespace to dewarp.py

--- denoise/denoise.py
+++ denoise/denoise.py
@@ -1,8 +1,10 @@
 import cv2 as cv
 
 '''Denoises an image. The output color scheme matches input (gray or RGB).'''
-def denoise(img, isRgb=False):
-    if isRgb:
+
+
+def denoise(img, is_rgb=False):
+    if is_rgb:
         img = cv.cvtColor(img, cv.COLOR_RGB2GRAY)
     cv.normalize(img, img, 0, 165, cv.NORM_MINMAX)
     img = cv.bitwise_not(img)
@@ -10,12 +12,14 @@ def denoise(img, isRgb=False):
     _, img = cv.threshold(img, 127, 0, cv.THRESH_TOZERO)
     img = cv.bitwise_not(img)
     img = cv.fastNlMeansDenoising(img, None, 20, 7, 21)
-    if isRgb:
+    if is_rgb:
         img = cv.cvtColor(img, cv.COLOR_GRAY2RGB)
     return img
 
-def denoise_at(path, isRgb=False):
-    return denoise(cv.imread(path, cv.IMREAD_GRAYSCALE), isRgb)
+
+def denoise_at(path, is_rgb=False):
+    return denoise(cv.imread(path, cv.IMREAD_GRAYSCALE), is_rgb)
+
 
 if __name__ == "__main__":
     cv.imshow('output', denoise_at('/Users/kyle/Desktop/test.png'))
